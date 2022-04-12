c3717a6e6e837f70ae24525aa487261a93914696
Oleg Osipov <vopisso@gmail.com>
2022-04-12 22:31:07 +0300

    Many changes

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = { 11, 21, 31, 411, 15, 61, 17, 18, 19 };
+
+int max1 = Max(array[0], array[1], array[2]);
+int max2 = Max(array[3], array[4], array[5]);
+int max3 = Max(array[6], array[7], array[8]);
+
+int supermax = Max(max1, max2, max3);
+
+Console.WriteLine(supermax);
